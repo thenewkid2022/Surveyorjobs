97788e2ac2048c41e3d21a1063ae7475be76359e
Christian von Ow <christianvonow@Christians-MacBook-Pro.local>
2025-05-13 14:59:01 +0700

    Update next.config.js Konfiguration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@shared': path.join(__dirname, '../shared'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
